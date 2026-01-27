0cca9dad53dcc9c8790ef9a8a28bc94b56292d26
Marino Bonetti <marinobonetti@gmail.com>
2026-01-27 22:18:12 +0100

    fix: Receive sent comand to know how to parse

--- src/tag_iterator.rs
+++ src/tag_iterator.rs
@@ -1,26 +1,26 @@
+use crate::connector::{Connector, ConnectorError};
+use crate::frame::{Command, CommandResult};
+use crate::tag::Tag;
+use log::{debug, error};
 use std::collections::VecDeque;
 use std::io::{Read, Write};
-use crate::tag::Tag;
 use std::time::{Duration, Instant};
-use log::{debug, error};
-use crate::connector::{Connector, ConnectorError};
-use crate::frame::CommandResult;
 
 pub struct TagIterator<'a, P: Read + Write> {
     connector: &'a mut Connector<P>,
+    sent_command: &'a Command,
     counter: u32,
     last_emit: Instant,
     interval: Duration,
-    buffer: VecDeque<Tag>
+    buffer: VecDeque<Tag>,
 }
 impl<P> Iterator for TagIterator<'_, P>
-    where
+where
     P: Read + Write,
 {
     type Item = Result<Tag, ConnectorError>;
 
     fn next(&mut self) -> Option<Self::Item> {
-
         // Nel caso abbiamo bufferizzato multipli tags
         if let Some(tag) = self.buffer.pop_front() {
             return Some(Ok(tag));
@@ -36,7 +36,7 @@ impl<P> Iterator for TagIterator<'_, P>
 
         self.last_emit = Instant::now();
 
-        match self.connector.read_command(){
+        match self.connector.read_command(self.sent_command) {
             Ok(response) => {
                 debug!("Risposta ricevuta: {response}\n");
                 if let CommandResult::ResponsePackets(Ok(setted_values)) = response {
@@ -47,19 +47,21 @@ impl<P> Iterator for TagIterator<'_, P>
                 error!("Risposta ricevuta con errori {e}");
             }
         }
-        self.buffer
-            .pop_front()
-            .map(Ok)
-
+        self.buffer.pop_front().map(Ok)
     }
 }
 
-pub(crate) fn tag_stream<P: std::io::Read + std::io::Write>(connector: &mut Connector<P>, interval: Duration) -> TagIterator<P> {
+pub(crate) fn tag_stream<'a, P: std::io::Read + std::io::Write>(
+    connector: &'a mut Connector<P>,
+    sent_command: &'a Command,
+    interval: Duration,
+) -> TagIterator<'a, P> {
     TagIterator {
         connector,
+        sent_command,
         counter: 0,
         last_emit: Instant::now(),
         buffer: VecDeque::new(),
-        interval
+        interval,
     }
 }
