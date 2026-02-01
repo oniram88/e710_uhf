53657341e169a64b25bf49730fb0ac811e24ff45
Marino Bonetti <marinobonetti@gmail.com>
2026-02-01 21:32:21 +0100

    fix: For faster reading no phase

--- src/connector.rs
+++ src/connector.rs
@@ -329,7 +329,7 @@ where
         &mut self,
         antenna_cfg: Vec<(u8, u8)>,
     ) -> Result<TagIterator<'_, P>, ConnectorError> {
-        let cmd = Command::FastSwitchAntInventory(antenna_cfg, 0, Session::S1, Target::A, PhaseStatus::On, 1);
+        let cmd = Command::FastSwitchAntInventory(antenna_cfg, 0, Session::S1, Target::A, PhaseStatus::Off, 1);
 
         let iter_tag = tag_iterator::tag_stream(self, cmd, std::time::Duration::from_secs(0));
 
